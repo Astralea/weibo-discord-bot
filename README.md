45ace384c1a883d49611feb1e11d011448326c19
Ace Taffy <xucgphi@gmail.com>
2023-08-05 00:18:53 +0900

    major update

--- README.md
+++ README.md
@@ -1,38 +1,116 @@
 # Weibo-Discord-Bot
-我永遠喜歡永雛塔菲
+<!-- <h1 style="color:pink;">我永遠喜歡永雛塔菲</h1> -->
+**我永遠喜歡永雛塔菲**
+*Read this in other languages: [简体中文](README.zh-CN.md)*
 
-## Workflow of the bot
+This project is a Python-based bot that scans specified Weibo accounts and sends new posts to corresponding Discord channels. It's a great tool if you want to keep track of certain Weibo accounts and have updates delivered directly to your Discord server.
 
-Logic flow:
-- See if retweeted
-  - If retweeted, send the retweet
-  - If not retweeted:
-    - See if there is a picture
-      - If there is a picture, send the picture
-      - If not, send the text
+## Features
 
-### For images
+* Scans multiple Weibo accounts for new posts at scheduled intervals.
+* Sends new posts to corresponding Discord channels configured for each Weibo account.
+* Handles different types of Weibo posts including:
+  * Text only posts.
+  * Posts with images (single or multiple).
+  * Posts with video content.
+  * Posts with retweeted content.
+* If a post contains multiple images, it creates a collage of the images. Each GIF file will be send separately following the embed message.
+* Stores the ID of each processed Weibo post in a SQLite database to prevent duplication.
+* Sends a status update to a specified Discord channel every scheduled hours to indicate that the script is still running.
+* Uses Discord Webhook to deliver posts to Discord.
 
-* If only one image, send the image directly
-* If multiple images, send the collage of the images
-  * Will send each gif separately following the embed message
+## Workflow
 
-## Prompts
-Act as an experienced python engineer, write a script to do the following tasks:
-1. We will first load necessary urls from the .env file as below:
-   ```python
-    from dotenv import load_dotenv
-    import os
+1. The bot first retrieves all contents from the Weibo AJAX request URL of each Weibo account.
+2. For each retrieved post, it checks if the ID of the post is already in the SQLite database.
+   * If the ID is in the database, the bot skips this post.
+   * If the ID is not in the database, indicating it's a new post, the bot proceeds with the following steps:
+     * Checks whether the post is a retweet.
+       * If the post is a retweet, it sends the retweeted content to the configured Discord channel.
+       * If the post is not a retweet, it checks whether the post contains images or video.
+         * If the post contains a single image, it sends the image directly to the Discord channel.
+         * If the post contains multiple images, it creates a collage of the images and sends it to the Discord channel. Each GIF found among the images is sent separately following the embed message.
+         * If the post contains a video, it sends the video to the Discord channel.
+         * If the post does not contain any images or video, it sends the text of the post to the Discord channel.
+3. The bot records the ID of each new Weibo post in a SQLite database to prevent sending duplicate posts.
+4. Every scheduled hours, the bot sends a status update to a specified Discord channel to indicate that it is still running.
 
-    # Load the .env file
-    load_dotenv()
+These operations are performed continuously according to the configured schedule.
 
-    # Now you can access the variables as environment variables
-    weibo_url = os.getenv('WEIBO_URL')
-    message_webhook_url = os.getenv('MESSAGE_WEBHOOK_URL')
-    status_webhook_url = os.getenv('STATUS_WEBHOOK_URL')
-    ```
-2. For every X minutes, parse the content of the weibo_url, and send any new weibo posts to message_webhook_url. Notice that we can have both text and image posts, embed them using discord's embed feature.
-3. For every 1 hour, send a signal to status_webhook_url to indicate that the script is still running.
-4. The script should be able to run on both MacOS and Linux and Windows.
-5. Use pm2 to manage the script, and make sure it can restart automatically when it crashes.
+## Requirements
+
+* Python 3.7 or above
+* Selenium
+* Requests
+* Discord Webhook
+* Schedule
+* Pytz
+* SQLite3
+* TOML
+
+You can install these packages using pip:
+```bash
+  pip install -r requirements.txt
+```
+
+## Usage
+
+1. Clone this repository.
+
+```bash
+  git clone https://github.com/uiharu-kazari/weibo-discord-bot.git
+  cd weibo-discord-bot
+```
+
+2. Install the required packages.
+
+```bash
+  pip install -r requirements.txt
+```
+
+3. Configure a `config.toml` file with your Weibo and Discord information. Please refer to `config_sample.toml` file and fill in your details.
+   
+4. Run the script.
+
+```bash
+  python app.py
+```
+
+For `pm2`, configure the `ecosystem.config.js` file then run
+```bash
+  pm2 start ecosystem.config.js
+```
+For more details about pm2, check [pm2 documentation](https://pm2.keymetrics.io/docs/usage/quick-start/).
+
+## Obtaining the AJAX URL from Weibo
+
+To get the AJAX URL for a specific Weibo account, you need to perform the following steps:
+
+1. Open the Weibo page of the account in a web browser.
+2. Open the Network tab in the browser's Developer Tools.
+3. Reload the Weibo page.
+4. In the Network tab, look for a request of name `mymblog`. This is usually an XHR request.
+5. Right-click this request and select "Copy > Copy link address" (the exact wording may vary depending on your browser).
+6. The copied URL is the AJAX URL for this Weibo account. Paste this URL into the `config.toml` file.
+
+For example, the AJAX URL could look like this: 
+
+```url
+https://weibo.com/ajax/statuses/mymblog?uid=7618923072&page=1&feature=0
+```
+
+In this URL:
+
+* `uid` represents the User ID of the Weibo account. This is the user ID of the Weibo account you want to monitor.
+* `page` represents the page number of the Weibo account's posts. Usually, you would set this to 1 to get the most recent posts.
+* `feature` is a filter used to specify the type of posts you want to retrieve. For instance, `feature=0` generally returns all types of posts, `feature=1` returns original posts (not including reposts or retweets), `feature=2` returns image posts, `feature=3` returns video posts, `feature=4` returns music posts. You can adjust this parameter according to your needs.
+
+Please note that the AJAX URL can change over time or depending on the Weibo platform updates. If the bot stops working, you might need to update the AJAX URL.
+
+
+## Contributing
+Contributions are welcome! Please feel free to submit a Pull Request.
+
+## License
+
+This project is licensed under the terms of the MIT license. For more details, see the [LICENSE](LICENSE) file in the repository.
--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import time
 import os
 import json
 import sqlite3
-from dotenv import load_dotenv
+
 import requests
 from datetime import datetime
 import schedule
@@ -17,26 +17,37 @@ import platform
 import uuid
 from discord_webhook import DiscordWebhook, DiscordEmbed
 from pathlib import Path
+from image_collage import combine_images, resize_gif
+from typing import List
+import IPython
 
 
-# Load the .env file
-load_dotenv()
+# # from dotenv import load_dotenv, find_dotenv
+# # # Load the .env file
+# # load_dotenv(find_dotenv())
 
-# Access the variables as environment variables
-WEIBO_AJAX_URL = os.getenv('WEIBO_AJAX_URL')
-WEIBO_URL = os.getenv('WEIBO_URL')
-MESSAGE_WEBHOOK_URL = os.getenv('MESSAGE_WEBHOOK_URL')
-STATUS_WEBHOOK_URL = os.getenv('STATUS_WEBHOOK_URL')
+# # Access the variables as environment variables
+# # WEIBO_AJAX_URL = "https://weibo.com/ajax/statuses/mymblog?uid=6593199887&page=1&feature=0"#
+# WEIBO_AJAX_URL = os.getenv('WEIBO_AJAX_URL')
+# WEIBO_URL = os.getenv('WEIBO_URL')
+# MESSAGE_WEBHOOK_URL = os.getenv('MESSAGE_WEBHOOK_URL')
+# STATUS_WEBHOOK_URL = os.getenv('STATUS_WEBHOOK_URL')
+
+import toml
+
+# Load TOML data from a file
+CONFIG = toml.load('config.toml')
 
 
 class WeiboScrapper:
-    def __init__(self):
+    def __init__(self,account_names: List[str]):
         # Setup driver
         # add headless
         self.driver = self.new_driver()
         # create a sqlite database to store id
         # change to mongodb later
         self.db = sqlite3.connect('weibo.db')
+        # let's hope weibo id is unique across all accounts
         self.cursor = self.db.cursor()
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS weibo (id INTEGER PRIMARY KEY)''')
         self.db.commit()
@@ -47,11 +58,14 @@ class WeiboScrapper:
         self.kawaii_titles = data['kawaii_titles']
         self.should_delete_images = True
         # create a folder called images in the local directory if it does not exist
-        self.image_dir='images'
-        if not os.path.exists(self.image_dir):
-            os.mkdir(self.image_dir)
-        dbs_dir_path = Path("databases")
-        dbs_dir_path.mkdir(exist_ok=True)
+        if IPython.get_ipython() is None:
+            # Not in Jupyter, use __file__
+            self.image_dir = Path(__file__).parent / 'images'
+        else:
+            # In Jupyter, use the current working directory
+            self.image_dir = Path(IPython.get_ipython().magic('pwd')) / 'images'
+        self.image_dir.mkdir(exist_ok=True)
+        self.account_names = account_names
 
 
     
@@ -68,18 +82,18 @@ class WeiboScrapper:
 
     def start(self):
         # Run the scan immediately and then every 10 minutes
-        self.scan()
-        schedule.every(10).minutes.do(self.scan)
-
+        for account in self.account_names:
+            self.scan(CONFIG['weibo'][account])
+        schedule.every(10).minutes.do(lambda: [self.scan(CONFIG['weibo'][account]) for account in self.account_names])
         # Run send_status immediately and then every hour
-        self.send_status()
-        schedule.every(6).hours.do(self.send_status)
+        self.send_status(CONFIG['status']['message_webhook'])
+        schedule.every(6).hours.do(self.send_status,CONFIG['status']['message_webhook'])
 
         while True:
             schedule.run_pending()
             time.sleep(1)
         
-    def get_weibo_content_once(self):
+    def get_weibo_content_once(self,endpoints):
         # check if the driver is alive
         if self.driver.service.is_connectable():
             pass
@@ -88,7 +102,7 @@ class WeiboScrapper:
             self.driver = self.new_driver()
 
         try:
-            self.driver.get(WEIBO_AJAX_URL)
+            self.driver.get(endpoints['ajax_url'])
             # Wait for the dynamic content to load
             time.sleep(10)
             self.driver.implicitly_wait(20)
@@ -100,7 +114,7 @@ class WeiboScrapper:
         content = json.loads(json_text) # content is a dictionary
         return content['data']['list']
     
-    def check_id(self,item):
+    def check_id(self,item,endpoints):
         # if id is not in the database, return True
         # else return False
         weibo_item_id=item['id']
@@ -111,13 +125,25 @@ class WeiboScrapper:
             self.db.commit()
             return True
         else:
-            return False       
+            return False
 
-    def get_weibo_content_loop(self):
+    def add_all_id_to_db(self,endpoints):
+        # add all id to the database
+        # this is to prevent the program from sending old weibo
+        content = self.get_weibo_content_loop(endpoints)
+        if content:
+            for item in content:
+                self.cursor.execute('''INSERT INTO weibo (id) VALUES (?)''',(item['id'],))
+                self.db.commit()
+        else:
+            print('failed to get content')
+            return None       
+
+    def get_weibo_content_loop(self,endpoints):
         i=0
         print(f'getting weibo content... @ {datetime.now()}')
         while True:
-            content = self.get_weibo_content_once()
+            content = self.get_weibo_content_once(endpoints)
             if content:
                 break   
             print('retrying...')
@@ -129,52 +155,56 @@ class WeiboScrapper:
                 return None
         return content
 
-    def scan(self):
-        content = self.get_weibo_content_loop()
+    def scan(self,endpoints):
+        content = self.get_weibo_content_loop(endpoints)
         if content:
             # in reverse order
             for item in reversed(content):
-                if self.check_id(item):
-                    self.parse_item(item)
+                if self.check_id(item,endpoints):
+                    self.parse_item(item,endpoints)
                     time.sleep(5)
         else:
             print('failed to get content')
             return None
-        
-
-    def image_download(self,url:str):
-        response = requests.get(url) #stream value doesn't matter
+    
+    def image_download(self, url: str):
+        response = requests.get(url)
         if response.status_code == 200:
-            # use uuid to generate a random file name with the same extension
-            file_name = str(uuid.uuid4()) + os.path.splitext(url)[1]
-            file_path = os.path.join(self.image_dir,file_name)
-            with open(file_path, 'wb') as f:
-                f.write(response.content)
+            # Use uuid to generate a random file name with the same extension
+            file_name = str(uuid.uuid4()) + Path(url).suffix
+            file_path = Path('images') / file_name
+            file_path.write_bytes(response.content)
             return file_path
         else:
             print("Unable to download image. HTTP response code:", response.status_code)
+            return None
 
-    def images_download(self,URLs:list):
-        # download all images in the list to the local folder ./images
-        # return a list of local file paths
-        file_paths = []
-        for url in URLs:
-            file_path = self.image_download(url)
-            file_paths.append(file_path)
-        return file_paths
+    def images_download(self, URLs: list):
+        # Download all images in the list to the local folder ./images
+        # Return a list of local file paths
+        return [self.image_download(url) for url in URLs]
 
-    def images_delete(self,file_paths:list):
-        # delete all images in the list
-        # notice that file_paths could be status code
+    def images_delete(self, file_paths: list):
+        # Delete all images in the list
+        # Notice that file_paths could be status code
         for file_path in file_paths:
-            if type(file_path) == str:
-                os.remove(file_path)
+            if isinstance(file_path, (str, Path)):
+                try:
+                    Path(file_path).unlink()
+                    # print(f"File {file_path} deleted.")
+                except FileNotFoundError:
+                    pass
+                    # print(f"File {file_path} not found.")
+            else:
+                print(f"Invalid file path: {file_path}")
 
-    def parse_item(self,item):
-        self.webhook_message = DiscordWebhook(url=MESSAGE_WEBHOOK_URL)
+
+    def parse_item(self,item,endpoints):
+        self.webhook_message = DiscordWebhook(url=endpoints['message_webhook'])
         text_raw = item['text_raw']
         created_at = item['created_at']
-        title="塔菲の新微博喵~"
+        # title="塔菲の新微博喵~"
+        title = endpoints['title']
         source= item['source']
         # use discord embed to display the content
         embed_color = 16738740
@@ -182,19 +212,42 @@ class WeiboScrapper:
         discord_timestamp = dt.timestamp()
         # create the webhook
         # create the embed object
-        embed = DiscordEmbed(title=title, description=text_raw, color=embed_color, url=WEIBO_URL)
+        embed = DiscordEmbed(title=title, description=text_raw, color=embed_color, url=endpoints['read_link_url'])
         embed.set_footer(text=f"来自 {source}")
         embed.set_timestamp(discord_timestamp)
-        if 'pic_infos' in item.keys():
-            return self.parse_item_with_images(item,embed)
-        elif 'page_info' in item.keys():
-            return self.parse_item_with_video(item,embed)
-        elif 'retweeted_status' in item.keys():
-            return self.parse_item_retweet(item,embed)
+        if 'retweeted_status' in item.keys():
+            return self.parse_item_retweet(item,embed,endpoints)
         else:
-            return self.parse_item_text_only(item,embed)
+            if 'pic_infos' in item.keys():
+                return self.parse_item_with_images(item,embed,endpoints)
+            elif 'page_info' in item.keys():
+                if 'media_info' in item['page_info'].keys():
+                    return self.parse_item_with_video(item,embed,endpoints)
+                else:
+                    if 'page_pic' in item['page_info'].keys():
+                        return self.parse_item_with_page_pic(item,embed,endpoints)
+                    else:
+                        from uuid import uuid4
+                        # write the whold item to a json file
+                        with open(f'{str(uuid4())[-10:]}.json', 'w') as f:
+                            json.dump(item, f)
+                        raise Exception('Unknown page_info')
+            else:
+                return self.parse_item_text_only(item,embed,endpoints)
+    
+    def parse_item_with_page_pic(self, item, embed,endpoints):
+        image_url = item['page_info']['page_pic']
+        image_fp = self.image_download(image_url)
+        with image_fp.open('rb') as f:
+            self.webhook_message.add_file(file=f.read(), filename=image_fp.name)
+        embed.set_image(url=f'attachment://{image_fp.name}')
+        self.webhook_message.add_embed(embed)
+        response = self.webhook_message.execute()
+        if self.should_delete_images:
+            self.images_delete([image_fp])
+        return response.status_code
 
-    def parse_item_text_only(self, item, embed):
+    def parse_item_text_only(self, item, embed, endpoints):
         # add the embed object to the webhook
         self.webhook_message.add_embed(embed)
         # execute the webhook
@@ -202,29 +255,56 @@ class WeiboScrapper:
         # check the response
         return response.status_code
     
-    def parse_item_with_images(self, item, embed):
-        #TODO: collage the images
+    
+    def parse_item_with_images(self, item, embed, endpoints):
         image_urls = [v['original']['url'] for k,v in item['pic_infos'].items()]
         image_filepaths = self.images_download(image_urls)
         # use discord embed to display the content
-        image_filenames = [os.path.basename(image_filepath) for image_filepath in image_filepaths]
-        
-        image_name = image_filenames[0]
-        with open(os.path.join(self.image_dir,image_name ), "rb") as f:
-            self.webhook_message.add_file(file=f.read(), filename=image_name )
-        embed.set_image(url=f'attachment://{image_name}')
-
-
+        # image_filenames = [os.path.basename(image_filepath) for image_filepath in image_filepaths]
+        if len(image_filepaths) == 1:
+            collage_image_path = image_filepaths[0]
+        else:
+            collage_image_path = combine_images(image_filepaths)
+        with collage_image_path.open("rb") as f:
+            self.webhook_message.add_file(file=f.read(), filename=collage_image_path.name)
+        embed.set_image(url=f'attachment://{collage_image_path.name}')
         self.webhook_message.add_embed(embed)
         response = self.webhook_message.execute()
+        if len(image_filepaths) > 1:
+            time.sleep(1)
+            self.send_animated_images(image_filepaths,endpoints)
         if self.should_delete_images:
             self.images_delete(image_filepaths)
+            self.images_delete([collage_image_path])
         return response.status_code
 
-    def parse_item_with_video(self,item, embed):
-        video_url=item['page_info']['media_info']['stream_url']
+    def send_animated_images(self, gif_candidates_paths, endpoints):
+        # send a gif using self.webhook_message
+        # return the status code of the request
+        gif_webhook = DiscordWebhook(url=endpoints['message_webhook'])
+        files_to_delete = []
+        for gif in gif_candidates_paths:
+            if gif.suffix == ".gif":
+                while gif.stat().st_size / (1024 ** 2) > 8:
+                    # If the gif is larger than 8MB, resize it
+                    gif = resize_gif(gif)
+                    files_to_delete.append(gif)
+                with gif.open("rb") as f:
+                    gif_webhook.add_file(file=f.read(), filename=gif.name)
+        # execute only if there is a gif
+        if gif_webhook.files:
+            response = gif_webhook.execute()
+            if self.should_delete_images:
+                self.images_delete(files_to_delete)
+            return response.status_code
+        else:
+            return 204 # stands for no content
+         
 
-        video_webhook = DiscordWebhook(url=MESSAGE_WEBHOOK_URL,content=video_url)
+
+    def parse_item_with_video(self,item, embed,endpoints):
+        video_url=item['page_info']['media_info']['stream_url']
+        video_webhook = DiscordWebhook(url=endpoints['message_webhook'],content=video_url)
         self.webhook_message.add_embed(embed)
         response1 = self.webhook_message.execute()
         response2 = video_webhook.execute()
@@ -237,21 +317,34 @@ class WeiboScrapper:
         else:
             return response1.status_code
     
-    def parse_item_retweet(self,item, embed):
-        #TODO: add images
-        # content[7]['retweeted_status']['pic_infos']
+    def parse_item_retweet(self,item, embed, endpoints):
         retweet_text = item['retweeted_status']['text_raw']
         user_name=item['retweeted_status']['user']['screen_name']
+        if 'pic_infos' in item['retweeted_status'].keys():
+            image_urls=[v['original']['url'] for k,v in item['retweeted_status']['pic_infos'].items()]
+            image_filepaths = self.images_download(image_urls)
+            if len(image_filepaths) == 1:
+                collage_image_path = image_filepaths[0]
+            else:
+                collage_image_path = combine_images(image_filepaths)
+        if 'collage_image_path' in locals():
+            # add the image to embed field
+            with collage_image_path.open("rb") as f:
+                self.webhook_message.add_file(file=f.read(), filename=collage_image_path.name)
+            embed.set_image(url=f'attachment://{collage_image_path.name}')
         embed.add_embed_field(name=user_name, value=retweet_text)
         self.webhook_message.add_embed(embed)
         response = self.webhook_message.execute()
+        if self.should_delete_images and 'image_filepaths' in locals():
+            self.images_delete(image_filepaths)
+            self.images_delete([collage_image_path])
         return response.status_code
         
 
-    def send_status(self):
+    def send_status(self,status_webhook_url):
         # send status to discord, say that the script is running, add some random kawaii emoji and text
         # use discord embed to display the content
-        self.webhook_status = DiscordWebhook(url=STATUS_WEBHOOK_URL)
+        self.webhook_status = DiscordWebhook(url=status_webhook_url)
         embed_color = 16738740
         emoji = random.choice(self.kawaii_emojis)
         text = random.choice(self.kawaii_texts)
@@ -268,8 +361,7 @@ class WeiboScrapper:
 
         embed = DiscordEmbed(title=title, \
                              description=f"{emoji} {text} @ {time_now} -- {machine_info}",\
-                                  color=embed_color, \
-                                    url=STATUS_WEBHOOK_URL)
+                                  color=embed_color)
         embed.set_timestamp()
         self.webhook_status.add_embed(embed)
         # execute the webhook
@@ -278,5 +370,7 @@ class WeiboScrapper:
         return response.status_code
     
 if __name__ == "__main__":
-    weibo_scrapper = WeiboScrapper()
+    accounts=['ace_taffy','genshin_impact']
+    # accounts=['ace_taffy']
+    weibo_scrapper = WeiboScrapper(accounts)
     weibo_scrapper.start()
